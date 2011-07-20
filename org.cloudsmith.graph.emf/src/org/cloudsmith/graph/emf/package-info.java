714347ed68a0f7262ad71f9e1c530612c40ef294
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2011-07-20 03:21:32 +0200

    Added package-info file

---
+++ org.cloudsmith.graph.emf/src/org/cloudsmith/graph/emf/package-info.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2011 Cloudsmith Inc. and other contributors, as listed below.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Cloudsmith
+ * 
+ */
+/**
+ * This package contains classes that translates an EMF ecore model instance into Graphviz dot 
+ * notation under control
+ * of a {@link org.cloudsmith.graph.graphcss.GraphCSS}. The classes in this package can be injected with 
+ * Google Guice, but can also be instantiated manually.
+ */
+package org.cloudsmith.graph.emf;
