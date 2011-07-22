8394af50f88dee801cf0df24ae9a6716b1d8a374
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2011-07-23 01:07:42 +0200

    Defined default root graph label style (top, verdana, 12, grey).

--- org.cloudsmith.graph/src/org/cloudsmith/graph/style/themes/DefaultStyleTheme.java
+++ org.cloudsmith.graph/src/org/cloudsmith/graph/style/themes/DefaultStyleTheme.java
@@ -21,6 +21,7 @@ import org.cloudsmith.graph.graphcss.IFunctionFactory;
 import org.cloudsmith.graph.graphcss.Rule;
 import org.cloudsmith.graph.graphcss.Select;
 import org.cloudsmith.graph.graphcss.StyleSet;
+import org.cloudsmith.graph.style.Alignment;
 import org.cloudsmith.graph.style.Arrow;
 import org.cloudsmith.graph.style.EdgeDirection;
 import org.cloudsmith.graph.style.IStyleFactory;
@@ -52,6 +53,18 @@ public class DefaultStyleTheme implements IStyleTheme {
 
 	public static final String THEME_EDGE_REFERENCE = "Reference";
 
+	public static final String COLOR__LIGHT_GREY = "#cccccc";
+
+	public static final String COLOR__MID_GREY = "#b3b3b3";
+
+	public static final String COLOR__DARK_GREY = "#929292";
+
+	public static final String COLOR__MID_BLUE = "#2180c7";
+
+	public static final String COLOR__LIGHT_GREY_BLUE = "#77a7c2";
+
+	public static final String COLOR__WHITE = "#ffffff";
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -67,14 +80,14 @@ public class DefaultStyleTheme implements IStyleTheme {
 		// SUBGRAPH
 		defaultRuleSet.addRule(Select.element(ElementType.graph).withStyles( //
 			styles.shapeBrush(LineType.dotted, 0.5, false, false), //
-			styles.lineColor("#77a7c2") // "grey/blue" color
+			styles.lineColor(COLOR__LIGHT_GREY_BLUE) // "grey/blue" color
 		));
 
 		// NODE
 		defaultRuleSet.addRule(Select.element(ElementType.vertex).withStyles( //
-			styles.color("#2180c7"), //
-			styles.fillColor("#ffffff"), //
-			styles.lineColor("#b3b3b3"), //
+			styles.color(COLOR__MID_BLUE), //
+			styles.fillColor(COLOR__WHITE), //
+			styles.lineColor(COLOR__MID_GREY), //
 			styles.fontFamily("Verdana"), //
 			styles.fontSize(8), //
 			styles.shape(NodeShape.rectangle), //
@@ -83,8 +96,8 @@ public class DefaultStyleTheme implements IStyleTheme {
 
 		// EDGE
 		defaultRuleSet.addRule(Select.element(ElementType.edge).withStyles( //
-			styles.color("#929292"), //
-			styles.lineColor("#b3b3b3"), //
+			styles.color(COLOR__DARK_GREY), //
+			styles.lineColor(COLOR__MID_GREY), //
 			styles.fontFamily("Verdana"), //
 			styles.fontSize(7), //
 			styles.lineBrush(LineType.solid, 0.5), //
@@ -115,7 +128,10 @@ public class DefaultStyleTheme implements IStyleTheme {
 		// a table with one row and one cell that gets the label of the element
 		labelFormatStyle.put(styles.labelFormat(//
 		styles.labelTable("Label", //
-			styles.labelRow("LabelRow", styles.labelCell("LabelCell", functionFactory.label())))));
+			styles.labelRow("LabelRow", //
+				styles.labelCell("LabelCell", functionFactory.label() //
+				))) //
+		));
 
 		StyleSet simpleLabelFormat = new StyleSet();
 		simpleLabelFormat.put(styles.labelFormat(styles.labelStringTemplate(functionFactory.label())));
@@ -125,7 +141,15 @@ public class DefaultStyleTheme implements IStyleTheme {
 		Collections.addAll(rules, //
 			Select.element(ElementType.vertex).withStyle(labelFormatStyle),//
 			Select.element(ElementType.edge).withStyle(simpleLabelFormat), //
-			Select.element(ElementType.graph).withStyle(labelFormatStyle));
+			Select.element(ElementType.graph).withStyle(labelFormatStyle) //
+		);
+
+		// The label of a graph
+		Collections.addAll(rules, //
+			Select.and(Select.table("Label"), Select.parent(Select.graph())).withStyles( //
+				styles.fontFamily("Verdana"), //
+				styles.fontSize(12), //
+				styles.color(COLOR__DARK_GREY)));
 
 		// The label is only rendered if there is some text to render
 		// (this avoids empty bordered or filled background areas)
@@ -135,13 +159,15 @@ public class DefaultStyleTheme implements IStyleTheme {
 			styles.cellPadding(0), //
 			styles.cellBorderWidth(0), //
 			styles.cellSpacing(0), //
-			styles.borderWidth(0)));
+			styles.borderWidth(0) //
+		));
 
 		// set TD valign to "bottom" (to make labels centered vertically in nodes)
 		//
 		Collections.addAll(rules, //
 			Select.element(ElementType.cell).withStyle(//
-				styles.verticalAlign(VerticalAlignment.bottom)));
+				styles.verticalAlign(VerticalAlignment.bottom) //
+			));
 
 		// // Default rules for HREF URL's
 		// StyleSet urlFormatStyle = StyleSet.withImmutableStyles(styles.href("#{element.urlString}"));
@@ -151,11 +177,18 @@ public class DefaultStyleTheme implements IStyleTheme {
 
 		// Root graph should be compound by default (allow head/tail clip on cluster border).
 		Collections.addAll(rules, //
-			Select.element(ElementType.graph).withStyle(styles.compound(true)));
+			Select.element(ElementType.graph).withStyle(//
+				styles.compound(true) //
+			), //
+				// place root graph label at top left position (the default for subgraphs).
+			Select.graph().withStyles(//
+				styles.align(Alignment.left), styles.verticalAlign(VerticalAlignment.top)));
 
 		// Containment class edges should have a diamond by default
 		Collections.addAll(rules, Select.edge(THEME_EDGE_CONTAINMENT).withStyles( // styles
-			styles.direction(EdgeDirection.both), styles.arrowTail(Arrow.diamond)));
+			styles.direction(EdgeDirection.both), //
+			styles.arrowTail(Arrow.diamond) //
+		));
 
 		defaultInstanceRuleSet = Collections.unmodifiableList(rules);
 		return defaultInstanceRuleSet;
